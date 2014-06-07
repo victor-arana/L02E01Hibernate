f79fc76e5afc4a22fc4143b39dcd84fce7f6f373
Victor José Arana Rodríguez <victor_arana@hotmail.com>
2014-06-07 15:14:54 -0500

    Declaración e Implementación de DAOs

--- src/main/java/com/victorarana/springmvc/dao/DAOVideo.java
+++ src/main/java/com/victorarana/springmvc/dao/DAOVideo.java
@@ -1,5 +1,49 @@
 package com.victorarana.springmvc.dao;
 
-public class DAOVideo implements DAOVideoInterface{
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.victorarana.springmvc.dto.DTOVideo;
+
+@Component("DAOVideo")
+public class DAOVideo implements DAOVideoInterface{
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public void createVideo(DTOVideo video) {
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		session.save(video);
+		session.getTransaction().commit();
+		session.close();		
+	}
+
+	@Override
+	public DTOVideo retrieveById(Integer videoId) {
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		DTOVideo video = (DTOVideo) session.get(DTOVideo.class, videoId);
+		session.getTransaction().commit();
+		session.close();		
+		
+		return video;
+	}
+
+	@Override
+	public boolean updateVideo(DTOVideo video) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteVideo(DTOVideo video) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- src/main/java/com/victorarana/springmvc/dao/DAOVideoInterface.java
+++ src/main/java/com/victorarana/springmvc/dao/DAOVideoInterface.java
@@ -1,5 +1,19 @@
 package com.victorarana.springmvc.dao;
 
-public interface DAOVideoInterface {
+import com.victorarana.springmvc.dto.DTOVideo;
 
+/**
+ * This interface defines the standard operations to be performed on a 
+ * model object(s)
+ * 
+ * @author Victor José Arana Rodríguez
+ * @since Junio, 2014
+ */
+
+public interface DAOVideoInterface {
+	
+	public void  createVideo(DTOVideo video);
+	public DTOVideo retrieveById(Integer videoId);
+	public boolean  updateVideo(DTOVideo video);
+	public boolean  deleteVideo(DTOVideo video);
 }
